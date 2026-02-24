70444dd3027ce3252f60d1dca6e94695dd989e77
kerem.sekerci <kerem@example.com>
2026-02-24 08:00:54 +0300

    fix: add missing import SwiftData to 4 sheet views
    AddActivitySheet, AddBehaviorSheet, AddDocumentSheet, AddFeedingSheet
    all use store.modelContext.insert() which requires SwiftData import

--- PetLogEvcilHayvanSaglik/Views/Sheets/AddActivitySheet.swift
+++ PetLogEvcilHayvanSaglik/Views/Sheets/AddActivitySheet.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import SwiftData
 
 struct AddActivitySheet: View {
     let store: PetStore
--- PetLogEvcilHayvanSaglik/Views/Sheets/AddBehaviorSheet.swift
+++ PetLogEvcilHayvanSaglik/Views/Sheets/AddBehaviorSheet.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import SwiftData
 
 struct AddBehaviorSheet: View {
     let store: PetStore
